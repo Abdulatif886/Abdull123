5378f130ab03f6e3585748789cda8d0618ae9a18
Abdulatif123 <sale4111@gmail.com>
2020-01-24 15:20:08 +0500

    I learn js amd I dont't do have work

---
+++ js.js
@@ -0,0 +1,29 @@
+/* var userNum = +prompt('Введите номер квартиры');
+
+if(userNum > 0 && userNum < 21){
+    alert('Первый Подъезд')
+}
+else if(userNum > 20 && userNum < 49){
+    alert('Второй Подъезд')
+}
+else if(userNum > 48 && userNum < 91){
+    alert('Третий Подъезд')
+}
+else {
+    alert('Такой крвартиры нет')
+} */
+/* function searchPorch (flat,numPorchs, flatsInOnePorch){
+    for (var numPorchs = 1; numPorchs <= numPorchs; numPorchs++){
+        
+        var minInPorchFlat = numPorchs * flatsInOnePorch - flatsInOnePorch + 1
+        var maxInPorchFlat = numPorchs * flatsInOnePorch
+        if (minInPorchFlat <= flat && maxInPorchFlat >= flat){
+            return numPorchs
+        }
+    }
+} */
+var John = ['John', 123456]
+var Jane = ['Jane', 654321]
+var Alex = ['Alex', 987654]
+
+var Users = [John,Jane,Alex]
